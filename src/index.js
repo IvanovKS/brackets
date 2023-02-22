cc87232776a3bb67d223315b18e4bc6e99a80a6b
IvanovKS <Skitchi@mail.ru>
2023-02-23 00:12:39 +0300

    feat: add several solutions

--- src/index.js
+++ src/index.js
@@ -1,26 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  const open_br = ['(', '[', '{', '|', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const open_br = [
+    "(",
+    "[",
+    "{",
+    "|",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+  ];
   const close_br = {
-    [')']: '(',
-    [']']: '[',
-    ['}']: '{',
-    ['|']: '|',
-    ['1']: '1',
-    ['2']: '2',
-    ['3']: '3',
-    ['4']: '4',
-    ['5']: '5',
-    ['6']: '6',
-    ['7']: '7',
-    ['8']: '8',
-    ['9']: '9'
+    [")"]: "(",
+    ["]"]: "[",
+    ["}"]: "{",
+    ["|"]: "|",
+    ["1"]: "1",
+    ["2"]: "2",
+    ["3"]: "3",
+    ["4"]: "4",
+    ["5"]: "5",
+    ["6"]: "6",
+    ["7"]: "7",
+    ["8"]: "8",
+    ["9"]: "9",
   };
+  // '[||||]'
+  // [
   let stack = [];
   for (let i = 0; i < str.length; i++) {
     let currentSymbol = str[i];
     if (open_br.includes(currentSymbol)) {
       stack.push(currentSymbol);
+      if (currentSymbol === '|') {
+        stack.pop();
+      }
     } else {
       if (stack.length === 0) {
         return false;
@@ -33,10 +52,5 @@ module.exports = function check(str, bracketsConfig) {
       }
     }
   }
-  const solution = stack.length;
-  // let www;
-  if (stack.length % 2 === 0) {
-    return true;
-  } 
-  // return stack.length === 0;
-}
+  return stack.length % 2 === 0;
+};
